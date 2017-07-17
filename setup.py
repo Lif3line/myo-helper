42ab0e2a77fb6075a0a4d3c02032908ce8701fdb
Adam Hartwell <adamhartwell2@gmail.com>
2017-07-17 14:55:40 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""Utiltiy functions for working with Myo Armband data."""
+
+from setuptools import setup, find_packages
+
+
+setup(name='myo_helper',
+      version='0.1',
+      description='Utiltiy functions for working with Myo Armband data',
+      author='Lif3line',
+      author_email='adamhartwell2@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      url='https://github.com/Lif3line/myo_helper',  # use the URL to the github repo
+      install_requires=[
+          'scipy',
+          'sklearn',
+          'numpy'
+      ],
+      keywords='ninapro emg')
